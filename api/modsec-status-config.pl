d6b7aef41593d566e602b5b35b33bd70ea408060
Felipe Zimmerle <fcosta@trustwave.com>
2014-01-15 17:03:41 -0300

    Adds api files

---
+++ api/modsec-status-config.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+
+our $mysql_dbi, $mysql_username, $mysql_password;
+
+$mysql_dbi = "dbi goes here";
+$mysql_username = "username goes here";
+$mysql_password = "password goes here";
+
+1;
---
+++ api/modsec-status.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+use DBI;
+
+require "modsec-status-config.pl";
+
+our $mysql_dbi, $mysql_username, $mysql_password;
+
+$dbh = DBI->connect($mysql_dbi, $mysql_username, $mysql_password) or die "Connection Error: $DBI::errstr\n";
+
+my $uri = $ENV{'REQUEST_URI'};
+my ($from, $to) = $uri =~ m/.*\/([\d+]+)\/([\d+]+)$/;
+
+$sql = "select ts,modsec,apache,apr,apr_loaded,pcre,pcre_loaded," .
+       "lua,lua_loaded,libxml,libxml_loaded,host,latitude,longitude, ".
+       "country,city,id,UNIX_TIMESTAMP(ts) from status where " .
+       "ts >= FROM_UNIXTIME($from) and ts < FROM_UNIXTIME($to) order by ts ASC LIMIT 0, 20000 ";
+
+$sth = $dbh->prepare($sql);
+$sth->execute or die "SQL Error: $DBI::errstr\n";
+
+print "Content-type: application/json\n\n";
+print "{\n";
+print ' "time": {' . "\n";
+print '  "from": "' . $from . '",' . "\n";
+print '  "to": "' . $to . '"' . "\n";
+print ' },' . "\n";
+print ' "amount": ' . $sth->rows . ",\n";
+print ' "results": [' . "\n";
+$count = 0;
+
+while (@row = $sth->fetchrow_array) {
+    print '  {' . "\n";
+    print '   "ts": "' . $row[0] . '"' . ",\n" if ($full);
+    print '   "ts_epoch": "' . $row[17] . '"' . ",\n";
+    print '   "version": "' . $row[1] . '"' . ",\n";
+    print '   "installation_id": "' . $row[16] . '"' . ",\n" if ($full);
+    print '   "apache": "' . $row[2] . '"' . ",\n" if ($full);
+    print '   "apr": {' . "\n" if ($full);
+    print '    "compiled": "' . $row[3] . '"' . ",\n" if ($full);
+    print '    "running": "' . $row[4] . '"' . "\n" if ($full);
+    print '   },' . "\n" if ($full);
+    print '   "pcre": {' . "\n" if ($full);
+    print '    "compiled": "' . $row[5] . '"' . ",\n" if ($full);
+    print '    "running": "' . $row[6] . '"' . "\n" if ($full);
+    print '   },' . "\n" if ($full);
+    print '   "lua": {' . "\n" if ($full);
+    print '    "compiled": "' . $row[7] . '"' . ",\n" if ($full);
+    print '    "running": "' . $row[8] . '"' . "\n" if ($full);
+    print '   },' . "\n" if ($full);
+    print '   "libxml": {' . "\n" if ($full);
+    print '    "compiled": "' . $row[9] . '"' . ",\n" if ($full);
+    print '    "running": "' . $row[10] . '"' . "\n" if ($full);
+    print '   },' . "\n" if ($full);
+    print '   "dns_server": {' . "\n";
+    print '    "ip": "' . $row[11] . '"' . ",\n" if ($full);
+    print '    "latitude": "' . $row[12] . '"' . ",\n";
+    print '    "longitude": "' . $row[13] . '"' . ",\n";
+    print '    "country": "' . $row[14] . '"' . ",\n" if (full);
+    print '    "city": "' . $row[15] . '"' . "\n";
+    print '   }' . "\n";
+    $count++;
+    if ($count == $sth->rows) {
+        print '  }' . "\n";
+    } else {
+        print '  },' . "\n";
+    }
+} 
+
+print ' ]' . "\n";
+print "}\n";
+
